bc4ad6419d2167bfdcbb6687938fc4a3cdf14681
lennard sdrojek <lennardsdrojek42@gmail.com>
2025-12-13 21:06:43 +0100

    made player page tested Reactbits and brainstoming

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
+import MagicBento from '@/components/MagicBento'
+
 
 export default function Player() {
   return (
-    <>
-      <h1>Player</h1>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/home">Homepage</Link>
-      </nav>
-    </>
+      <>
+          <h1>Player</h1>
+          <nav>
+              <Link href="/">Home</Link> | <Link href="/home">Homepage</Link>
+          </nav>
+          <body>
+              <p> Here comes text data and some cool animatons for the data and overview </p>
+          </body>
+      </>
   );
 }
